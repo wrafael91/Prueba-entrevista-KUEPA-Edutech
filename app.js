319ec661cd9c95bfb7b9b13e857984277db5fd67
wrafael91 <wrafaelmp91@gmail.com>
2022-06-09 01:29:55 -0500

    adding images and api connections

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 
 function personasAPI() {
-    const url1 = `https://api.opendota.com/api/proPlayers`
+    const url1 = `https://api.opendota.com/api/proPlayers`;
     fetch(url1)
         .then(respuesta => respuesta.json())
         .then(datos => mostrarLeadsObtenidos(datos))
 };
 
+function playersAPI(){
+    const url2 = `https://api.opendota.com/api/heroes`;
+    fetch(url2)
+        .then(respuesta2 => respuesta2.json())
+        .then(datos2 => mostrarPlayers(datos2))
+};
+
 function mostrarLeadsObtenidos(leads){
     const leadsObtenidos = document.querySelector('#leads_obtenidos');
+    const leadsMatriculas = document.querySelector('#leads_matriculas');
+    const colaLlamados = document.querySelector('#cola_llamados');
+
     let leadsSumatoria = '';
     leads.forEach(lead => {
         leadsSumatoria += lead
     });
-    leadsObtenidos.innerHTML = `50 / ${parseInt(leadsSumatoria.length*0.00243)}`;
+    
+    leadsObtenidos.innerHTML = `50 / ${parseInt(leadsSumatoria.length * 0.00243)}`;
+    leadsMatriculas.innerHTML = `10 / ${parseInt(leadsSumatoria.length * 0.00049)}`;
+    colaLlamados.innerHTML = `50 / ${parseInt(leadsSumatoria.length * 0.00243)}`;
+}
+
+function mostrarPlayers(players){
+    const playersPendientes = document.querySelector('#pendientes_ayer');
+
+    let tablaPlayers = [];
+    players.forEach(player => {
+        const {localized_name} = player
+        tablaPlayers += `
+            <tr>
+                <td>${localized_name}</td>
+                <td>Llamar hoy</td>
+            </tr>
+        `;
+    });
+    playersPendientes.innerHTML = tablaPlayers;
 }
 
 personasAPI();
+
+playersAPI();
